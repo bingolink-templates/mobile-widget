5384176613fb7e7cbb65e523351b40cbd03ac647
lufeng <lufeng@bingosoft.net>
2019-05-07 23:23:12 +0800

    inital commit

---
+++ src/views/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="center">
+        <bui-image src="/image/logo.png" width="120px" height="120px"></bui-image>
+        <text class="h2" style="color:red;">移动端小部件模板</text>
+        <text class="h4">BUI-Weex版</text>
+    </div>
+</template>
+
+<!--引入bui-weex样式文件-->
+<style lang="scss" src="bui-weex/src/css/buiweex.scss"></style>
+
+<script>
+    var globalEvent = weex.requireModule('globalEvent');
+    module.exports = {
+        data () {
+            return {}
+        },
+        mounted()
+        {
+        }
+    }
+</script>

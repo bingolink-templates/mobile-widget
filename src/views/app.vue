ae6941c5b333e6468bbb10e5a666f04753467e13
lufeng <lufeng@bingosoft.net>
2019-05-22 15:27:44 +0800

    broadcastWidgetHeight

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="center">
+    <div class="center" ref="wrap" style="height:100%;">
         <bui-image src="/image/logo.png" width="120px" height="120px"></bui-image>
         <text class="h2" style="color:red;">移动端小部件模板</text>
         <text class="h4">BUI-Weex版</text>
@@ -11,12 +11,27 @@
 
 <script>
     var globalEvent = weex.requireModule('globalEvent');
+    var dom = weex.requireModule('dom');
+
     module.exports = {
         data () {
             return {}
         },
         mounted()
         {
+            this.broadcastWidgetHeight()
+        },
+        methods: {
+            broadcastWidgetHeight(){
+                var _params = this.$getPageParams();
+                setTimeout(() => {
+                    dom.getComponentRect(this.$refs.wrap , (ret) => {
+                        var channel = new BroadcastChannel('WidgetsMessage')
+                        channel.postMessage({widgetHeight: ret.size.height, id: _params.id});
+                        channel.close();
+                    });
+                }, 100)
+            }
         }
     }
 </script>
